d4c899c1f9c060a254dda9ab8cc0d0703cc1b4b7
theforestvn88 <theforestvn88@gmail.com>
2023-10-30 19:42:04 +0700

    fix one-to-one connection

--- app/assets/javascripts/p2p_connection.js
+++ app/assets/javascripts/p2p_connection.js
@@ -2,7 +2,8 @@ import { MessageType } from "./message_type"
 
 const CONFIG = {
     iceServers: [
-		{ urls: "stun:stun.l.google.com:19302" },
+		{ urls: ["stun:stun.l.google.com:19302", "stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"]
+        },
 	],
 }
 
@@ -14,21 +15,32 @@ export default class P2pConnection {
         this.peerId = peerId
     }
     
-    signal(key, val) {
+    signal(type, data) {
         let msg = {
             "session": this.session,
-            "peerId": this.peerId,
-            "type": key
+            "peer_id": this.peerId,
+            "type": type,
+            ...data
         }
-        msg[key] = val
         console.log(msg)
         this.signaling.send(msg)
     }
 
     start() {
+        console.log("connection start ...")
         this.connection = new RTCPeerConnection(CONFIG)
         this.connection.onicecandidate = event => {
-            this.signal(MessageType.IceCandidate, event.candidate)
+            console.log(`onicecandidate`)
+            console.log(event)
+            if (event.candidate) {
+                let ice = {}
+                ice[MessageType.IceCandidate] = event.candidate
+                this.signal(MessageType.IceCandidate, ice)
+            }
+        }
+        this.connection.oniceconnectionstatechange = event => {
+            console.log(`oniceconnectionstatechange`)
+            console.log(event)
         }
         
         this.sendDataChannel = this.connection.createDataChannel("sendChannel");
@@ -36,14 +48,17 @@ export default class P2pConnection {
         this.sendDataChannel.onclose = this.handleSendChannelStatusChange;
 
         this.connection.ondatachannel = event => {
+            console.log("ondatachannel p2p ...")
             this.receiveDataChannel = event.channel
             this.receiveDataChannel.onmessage = this.receiveP2pMessage
             this.receiveDataChannel.onopen = this.handleReceiveChannelStatusChange
             this.receiveDataChannel.onclose = this.handleReceiveChannelStatusChange
+
+            this.sendDataChannel.send(`${this.peerId}: hi`)
         }
 
         console.log("SEND MessageType.SessionJoin")
-        this.signal(MessageType.SessionJoin, "")
+        this.signal(MessageType.SessionJoin, {})
     }
 
     handleConnectionMessage(data) {
@@ -58,7 +73,9 @@ export default class P2pConnection {
                             return this.connection.setLocalDescription(offer)
                         })
                         .then(() => {
-                            this.signal(MessageType.SdpOffer, JSON.stringify(this.connection.localDescription))
+                            let offer = {"host": data.host}
+                            offer[MessageType.SdpOffer] = JSON.stringify(this.connection.localDescription)
+                            this.signal(MessageType.SdpOffer, offer)
                         })
                         .catch(err => console.log(err))
                 }
@@ -66,14 +83,19 @@ export default class P2pConnection {
                 break
             case MessageType.SdpOffer:
                 if (data.host != this.peerId) { // iam not host
-                    this.connection.setRemoteDescription(new RTCSessionDescription(data[MessageType.SdpOffer]))
+                    let offer = JSON.parse(data[MessageType.SdpOffer])
+                    console.log(`${this.peerId} get Offer`)
+                    console.log(offer)
+                    this.connection.setRemoteDescription(new RTCSessionDescription(offer))
                         .then(() => {
                             this.connection.createAnswer()
                                 .then(answer => {
                                     return this.connection.setLocalDescription(answer)
                                 })
                                 .then(() => {
-                                    this.signal(MessageType.SdpOffer, JSON.stringify(this.connection.localDescription))
+                                    let answer = {"host": data.host}
+                                    answer[MessageType.SdpAnswer] = JSON.stringify(this.connection.localDescription)
+                                    this.signal(MessageType.SdpAnswer, answer)
                                 })
                                 .catch(err => console.log(err))
                         })
@@ -81,7 +103,10 @@ export default class P2pConnection {
                 break
             case MessageType.SdpAnswer:
                 if (data.host == this.peerId) { // iam host
-                    this.connection.setRemoteDescription(new RTCSessionDescription(data[MessageType.SdpAnswer]))
+                    let answer = JSON.parse(data[MessageType.SdpAnswer])
+                    console.log(` ${this.peerId} get Answer`)
+                    console.log(answer)
+                    this.connection.setRemoteDescription(new RTCSessionDescription(answer))
                         .catch(err => console.log(err))
                 }
                 break
@@ -98,7 +123,9 @@ export default class P2pConnection {
     }
 
     receiveP2pMessage(event) {
-        this.container.dispatchP2pMessage(event.data)        
+        console.log(`p2p received msg: ${event.data}`)
+        // TODO:
+        // this.container.dispatchP2pMessage(event.data)        
     }
 
     sendP2pMessage(message) {
@@ -106,10 +133,10 @@ export default class P2pConnection {
     }
 
     handleSendChannelStatusChange(event) {
-
+        console.log(event)
     }
 
     handleReceiveChannelStatusChange(event) {
-
+        console.log(event)
     }
 }
